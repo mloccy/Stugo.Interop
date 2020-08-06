4c4fbccaade55e278dd1ecd70afd40e3e643dab2
Louise Keegan <louise.keegan@unity3d.com>
2020-08-06 10:46:46 +0100

    It seems this is needed afterall.

--- Stugo.Interop/EntryPointAttribute.cs
+++ Stugo.Interop/EntryPointAttribute.cs
@@ -8,9 +8,14 @@ namespace Stugo.Interop
     /// <summary>
     /// Allows the specification of the unmanaged entry point.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Delegate)]
+    [AttributeUsage(AttributeTargets.Field)]
     public class EntryPointAttribute : Attribute
     {
+        public EntryPointAttribute(string entryPoint)
+        {
+            EntryPoint = entryPoint;
+        }
+
         public string EntryPoint { get; set; }
     }
 }
